821a7924e8b58de31170063f5c7a00fbead85dd0
Danilo <danibustillo97@gmail.com>
2024-03-07 16:15:50 -0500

    Primer beta V0.0.2 Beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
 
 function App() {
-   return <div className="bg-red-300">hola mundo</div>
- 
-
-
+   return (
+   <div className="bg-[#0B131A] min-h-screen grid  grid-cols-1 xl:grid-cols-4">
+      <div className="bg-[#1B2831] ">
+         <div>Fotos</div>
+         <div>Iconos</div>
+      </div>
+      <div className="xl:col-span-3">Hola Mundo 2</div>
+   </div>
+   
+   )
 }
 
 export default App
